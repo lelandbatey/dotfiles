3e8d0cebdd7e4efcfcc42420f1d0fd11e7a51743
Leland Batey <lelandbatey@lelandbatey.com>
2019-09-30 11:26:22 -0700

    Expand utc_info functionality

--- scripts/utc_info.sh
+++ scripts/utc_info.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-UTCNOW="$(TZ='UTC' date +'%a %b %d %H:%M:%S %p %Z %Y')"
+if [[ -z "$1" ]]; then
+	EPOCH_TIME="$(date "+%s")"
+else
+	EPOCH_TIME="$1"
+fi
 
-printf "Current time in UTC/GMT: $UTCNOW (24 hour time)\n"
-printf "                         $(TZ='UTC' date +'%a %b %d %I:%M:%S %p %Z %Y') (12 hour time)\n"
+if [[ "${#EPOCH_TIME}" -gt "10" ]]; then
+	EPOCH_TIME="$(printf "scale=5\n$EPOCH_TIME / 1000.00\n" | bc -l)"
+fi
+echo $EPOCH_TIME
+
+UTCNOW="$(TZ='UTC' date -d "@$EPOCH_TIME" +'%a %b %d %H:%M:%S %p %Z %Y')"
+
+printf "Time in UTC/GMT: $UTCNOW (24 hour time)\n"
+printf "                 $(TZ='UTC' date -d "@$EPOCH_TIME" +'%a %b %d %I:%M:%S %p %Z %Y') (12 hour time)\n"
 #printf "Current time here      : $(date)\n"
-printf "Current time here      : $(date +'%a %b %d %H:%M:%S %p %Z %Y')\n"
+printf "Time here      : $(date -d "@$EPOCH_TIME" +'%a %b %d %H:%M:%S %p %Z %Y')\n"
 
